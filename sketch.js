5ee4cca78d05b13425e1ff246a4a529ffac354cc
Colin Willoughby <39289465+colinwillowtree@users.noreply.github.com>
2022-02-05 17:10:27 -0800

    Test
    Test

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(800, 800);
+   rectMode(CENTER);
+}
+
+function draw() {
+  background(220);
+    var col1 = 0;
+  var col2 = 0;
+  col1 = int(map(mouseX,0,width,0,255));
+  col2 = int(map(mouseY,0,height,0,255));
+  fill(col1,col2,255);
+  rect(mouseX, mouseY,100,100);
+}
